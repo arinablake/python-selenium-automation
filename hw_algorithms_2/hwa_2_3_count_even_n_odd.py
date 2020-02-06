9f7b8206e0e087e54990904f7bd64acb968a9dfa
arinablake <arinafilippova@gmail.com>
2020-02-05 20:36:36 -0500

    Homework algorithms 2

--- hw_algorithms_2/hwa_2_3_count_even_n_odd.py
+++ hw_algorithms_2/hwa_2_3_count_even_n_odd.py
@@ -1,13 +1,12 @@
 # Посчитать четные и нечетные цифры числа. Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560, то у него 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
 
-num = int(input('Enter number '))
+num = input('Enter number ')
 
 def count_even_odd(num):
     even_count = 0
     odd_count = 0
-    lst = [int(i) for i in str(num)]
-    for i in lst:
-        if i % 2 == 0:
+    for i in num:
+        if int(i) % 2 == 0:
             even_count += 1
         else:
             odd_count += 1
